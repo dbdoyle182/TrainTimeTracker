d6f18aeac4fce7cda7eb83c5b024f6a7813347c1
Daniel Doyle <dbdoyle182@gmail.com>
2018-03-09 17:19:52 -0500

    added full html linked firebase began js

---
+++ assets/train.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAYorv_6NjJ4boN4-tNA9EpXY2sycHaMog",
+    authDomain: "train-tracker-d7a2e.firebaseapp.com",
+    databaseURL: "https://train-tracker-d7a2e.firebaseio.com",
+    projectId: "train-tracker-d7a2e",
+    storageBucket: "",
+    messagingSenderId: "245070538117"
+    };
+firebase.initializeApp(config);
+
+var database = firebase.database();
